fde7ecd8f7e9d2569cc9883c2e63984a5df284b7
Carlos Antón Mesa <cam1001@alum.ubu.es>
2021-04-10 18:44:40 +0200

    Añadimos método abstracto a la interface IParque

--- src/p03/c01/IParque.java
+++ src/p03/c01/IParque.java
@@ -2,8 +2,15 @@ package p03.c01;
 
 public interface IParque {
 	
+	/**
+	 * Definir método de entrada al parque
+	 * @param puerta de entrada
+	 */
 	public abstract void entrarAlParque(String puerta);
-
-	// TODO
+	/**
+	 * Definir método de salida del parque
+	 * @param puerta de salida del parque
+	 */
+	public abstract void salirDelParque(String puerta);
 
 }
